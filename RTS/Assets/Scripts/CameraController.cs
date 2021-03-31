ba697370d90699888f32ff0abe7b96620ec1be13
Melon332 <thederpking@outlook.com>
2021-03-31 13:41:00 +0200

    Added a click function to the camera.

---
+++ RTS/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Camera rtsCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rtsCamera = GetComponent<Camera>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClickedOnSomething();  
+    }
+
+    private void ClickedOnSomething()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = rtsCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Clicked " + hit.collider.gameObject.name);
+            }
+        }
+    }
+}
